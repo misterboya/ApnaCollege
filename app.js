1491422073c029f3963d989f169f0c90f790da18
misterboya <mady.mister@gmail.com>
2025-02-03 11:00:21 +0000

    test test

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ let age = 15;
 //ternary operator example
 let result = age > 18 ? "Adult" : "Not Adult";
 console.log(result);
-console.log("testing git worktree");
+console.log("testing git worktree again");
+console.log("test again with worktree");
